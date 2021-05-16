6cbe08378e616688c8793cd7de483a524a8f916f
Yongcheng Wu <yc_wu1030@126.com>
2021-05-16 01:22:51 -0500

    feat(logs):
    Adding log messages

--- include/EMD.h
+++ include/EMD.h
@@ -7,10 +7,10 @@ class EMD
 {
 private:
     REAL TInflation; // Temperature at the end of Inflation.
-    REAL Ti; // Temperature at the beginning of Early Matter Dominant Era 
-    REAL Te; // Temperature at which we start the Entropy Production period
-    REAL Tr; // Reheating Temperature at the end of the Early Matter Dominant Era (at the end of Entropy production)
-    REAL Tf; // Final Temperature where we stop the evolution.
+    REAL Ti;         // Temperature at the beginning of Early Matter Dominant Era
+    REAL Te;         // Temperature at which we start the Entropy Production period
+    REAL Tr;         // Reheating Temperature at the end of the Early Matter Dominant Era (at the end of Entropy production)
+    REAL Tf;         // Final Temperature where we stop the evolution.
 
     // D.O.F
     REAL gei, gsi;
@@ -24,6 +24,7 @@ private:
 
     void Get_Te();
     void Set_Temperature(REAL _Ti, REAL _Tr, REAL _Tf = 1e-3, REAL _TInflation = 1e15);
+
 public:
     EMD();
     EMD(REAL _Ti, REAL _Tr, REAL _Tf = 1e-3, REAL _TInflation = 1e15);
@@ -33,17 +34,32 @@ public:
     friend double Equation_For_LogTe(double logTe, void *param);
 };
 
-
-/*
- * @brief: Calculate the number density at equilibrium
- * @param T, the temperature
- * @param M, the mass of the species
- * @param g, the internal dof of the species 
-*/
+/**
+ * @brief  Calculate the particle number density at equilibrium
+ * @note
+ * @param  T: The temperature
+ * @param  M: The mass of the particle
+ * @param  g: The internal dof of the particle
+ * @retval The particle number density at equilibrium
+ */
 REAL Number_Density_Eq(REAL T, REAL M, REAL g);
 
+/**
+ * @brief  Calculate the Entropy density at Temperature T
+ * @note
+ * @param  T: The Temperature
+ * @retval The entropy density
+ */
 REAL Entropy_Density(REAL T);
 
+/**
+ * @brief Calculate the Yield (n/s) at equilibrium
+ * @note
+ * @param  T: The Temperature
+ * @param  M: The mass of the particle
+ * @param  g: The internal dof of the particle
+ * @retval The Yield at equilibrium
+ */
 REAL Yield_Eq(REAL T, REAL M, REAL g);
 
 #endif
