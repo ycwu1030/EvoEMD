7e765c0e5e66e3d05c12ab408b92ee477e0db8e3
Yongcheng Wu <yc_wu1030@126.com>
2021-10-20 14:03:48 -0500

    refactor(CollisionRate): amplitude calculation for 2->2 scattering
    Add function calculate the amplitude for 2->2 scattering, integrate over final state phase space

--- include/CollisionRate.h
+++ include/CollisionRate.h
@@ -1,20 +1,39 @@
 #ifndef _COLLISION_RATE_H_
 #define _COLLISION_RATE_H_
 
+#include <map>
+
 #include "Particles.h"
 #include "RealTypes.h"
 
 struct Process_Amp {
-    // * We consider 2 -> 2 processes or 1 -> 2 decays
-    // * 0 1 2 for s-channel, t-channel, u-channel
-    // * For one single diagram, it has three cases: s, t, u channel (contact is counted as s channel)
-    // * For squared, then it has 3x3 cases.
-    // * But in real calculation, we will add s-t and t-s together, so Amp_xxx[0][1] == s-t + t-s = Amp_xxx[1][0] etc
-    // * For decay process, treated as s-channel, [0][0] is used.
-    // * The last index is for store expression in cth expansion, [x][y][0] + [x][y][1]*cth + [x][y][2]*cth^2
-    REAL Amp_Numerator[3][3][3];
-    REAL Amp_Denominator[3][3][3];
+    typedef enum { SS = 0, ST = 1, SU = 2, TT = 3, TU = 4, UU = 5 } Channel;
+    static const Process_Amp::Channel All_Channel[6];
+    typedef std::map<Process_Amp::Channel, unsigned> AMP_DIAG;
+    AMP_DIAG n_diag;
+
+    // * bool determine whether the corresponding REAL is actually exactly zero
+    typedef std::pair<bool, REAL> CTH_RES;
+    typedef std::vector<CTH_RES> CTH_RES_FULL;
+    typedef CTH_RES_FULL NUMERATOR_STRUCTURE;
+    typedef int Propagator_ID;
+    typedef std::pair<Propagator_ID, CTH_RES_FULL> PROPAGATOR_STRUCTURE;
+    typedef std::pair<PROPAGATOR_STRUCTURE, PROPAGATOR_STRUCTURE> DENOMINATOR_STRUCTURE;
+
+    // * The key used to access the results: Channel + Diagram_ID;
+    typedef std::pair<Channel, int> AMP_KEY;
+
+    // * Key -> RES[3] for numerator: [0] + [1]*cth + [2]*cth^2;
+    // * Key -> RES[4] for denominator: ([0] + [1]*cth)*([2]+[3]*cth)
+    typedef std::map<AMP_KEY, NUMERATOR_STRUCTURE> AMP_NUM;
+    typedef std::map<AMP_KEY, DENOMINATOR_STRUCTURE> AMP_DEN;
+    AMP_NUM amps_numerator;
+    AMP_DEN amps_denominator;
+
+    NUMERATOR_STRUCTURE &Get_Numerator(Channel chan1 = Channel::SS, int diagram_id = 0) const;
+    DENOMINATOR_STRUCTURE &Get_Denominator(Channel chan1 = Channel::SS, int diagram_id = 0) const;
 };
+const Process_Amp::Channel Process_Amp::All_Channel[6] = {SS, ST, SU, TT, TU, UU};
 
 class Amplitude {
     // * Amplitude used to calculate the collision rate;
@@ -24,13 +43,20 @@ class Amplitude {
     // * 1. The spin dof: the amp is summed over spin dof for all external particles
     // * 2. The species dof: CP conjugated component is summed as 1 -> 2 3 is added with 1bar -> 2bar 3bar etc.
     // *                     If CP violating rate is needed, (1->23) - (1bar->2bar3bar) is provided
+    // *                     Flavor dof is also summed
 public:
     typedef std::vector<Pseudo_Particle *> INITIAL_STATES;
     typedef std::vector<Pseudo_Particle *> FINAL_STATES;
+    typedef enum { SS = 0, ST = 1, SU = 2, TT = 3, TU = 4, UU = 5 } Channel;
+    typedef enum { CONST = 0, LINEAR = 1, QUAD = 2 } CTH_ORDER;
 
     Amplitude();
-    virtual ~Amplitude();
-    virtual Process_Amp Get_Amp() = 0;
+    virtual ~Amplitude(){};
+    virtual void Update_Amp(REAL sqrt_shat) = 0;
+    virtual const Process_Amp &Get_Amp(REAL sqrt_shat) {
+        Update_Amp(sqrt_shat);
+        return amp_res;
+    };
     virtual REAL Get_Coeff() = 0;
 
     unsigned N_INITIAL;
@@ -38,6 +64,9 @@ public:
 
     unsigned N_FINAL;
     FINAL_STATES FINAL;
+
+private:
+    Process_Amp amp_res;
 };
 
 class Collision_Rate {
@@ -76,10 +105,14 @@ public:
 
 class Scatter22_Rate : public Collision_Rate {
 protected:
+    REAL Get_Amp_Integrate_over_Phase_Space_Single_Channel(const Process_Amp::NUMERATOR_STRUCTURE &numerator,
+                                                           const Process_Amp::DENOMINATOR_STRUCTURE &denominator);
+
 public:
     Scatter22_Rate(Amplitude *amp) : Collision_Rate(amp){};
     ~Scatter22_Rate(){};
 
+    virtual REAL Get_Amp_Integrate_over_Phase_Space(REAL sqrt_shat);
     virtual REAL Get_Collision_Rate(REAL T);
 };
 
--- src/CollisionRate.cpp
+++ src/CollisionRate.cpp
@@ -7,7 +7,6 @@
 
 REAL Decay12_Rate::Get_Amp_Integrate_over_Phase_Space(REAL sqrt_shat) {
     // * For  1 -> 2 3;
-    REAL sqrtshat = sqrt_shat;  // avoid unused warning
     REAL m1 = amp->INITIAL[0]->Get_Mass();
     REAL m2 = amp->FINAL[0]->Get_Mass();
     REAL m3 = amp->FINAL[1]->Get_Mass();
@@ -19,11 +18,12 @@ REAL Decay12_Rate::Get_Amp_Integrate_over_Phase_Space(REAL sqrt_shat) {
     REAL pf = sqrt(Kallen_Lam(m12, m22, m32)) / 2.0 / m1;
 
     // * Two body phase space volumn
+    const Process_Amp &res = amp->Get_Amp(sqrt_shat);
     REAL PS2 = 1.0 / (2.0 * m1) * pf / (16.0 * M_PI * M_PI * m1);
-    REAL RES = 2.0                                        // * From integral over cos(theta)
-               * 2.0 * M_PI                               // * From integral over phi
-               * (amp->Get_Amp().Amp_Numerator[0][0][0])  // * The amplitude
-               * PS2;                                     // * The two body phase space
+    REAL RES = 2.0                                // * From integral over cos(theta)
+               * 2.0 * M_PI                       // * From integral over phi
+               * (res.Get_Numerator()[0].second)  // * The amplitude
+               * PS2;                             // * The two body phase space
     return RES;
 }
 
@@ -35,4 +35,118 @@ REAL Decay12_Rate::Get_Collision_Rate(REAL T) {
     REAL k1z = gsl_sf_bessel_Kn(1, z);
     REAL RES = m1 * m1 * T / 2.0 / M_PI / M_PI * k1z;
     return RES;
+}
+
+REAL Scatter22_Rate::Get_Amp_Integrate_over_Phase_Space(REAL sqrt_shat) {
+    using NS = Process_Amp::NUMERATOR_STRUCTURE;
+    using DS = Process_Amp::DENOMINATOR_STRUCTURE;
+    const Process_Amp &amp_res = amp->Get_Amp(sqrt_shat);
+    REAL amp_total = 0;
+    for (auto &&chan : Process_Amp::All_Channel) {
+        unsigned n_diag = amp_res.n_diag.at(chan);
+        for (unsigned i_diag = 0; i_diag < n_diag; i_diag++) {
+            NS &num = amp_res.Get_Numerator(chan, i_diag);
+            DS &den = amp_res.Get_Denominator(chan, i_diag);
+            amp_total += Get_Amp_Integrate_over_Phase_Space_Single_Channel(num, den);
+        }
+    }
+    return amp_total;
+}
+
+inline REAL INT_RES_00(REAL a, REAL b, REAL c, REAL d, REAL e) {
+    // * Int[(a+b x+ c x^2)/d/e,{x,-1,1}]
+    return 2 * (3 * a + c) / 3.0 / d / e;
+}
+inline REAL INT_RES_01(REAL a, REAL b, REAL c, REAL d, REAL e, REAL f) {
+    // * Int[(a+b x+ c x^2)/(d+ e x)/f, {x,-1,1}]
+    REAL den = pow(e, 3) * f;
+    REAL num = 2 * e * (b * e - c * d) - log((d - e) / (d + e)) * (e * (a * e - b * d) + c * d * d);
+    return num / den;
+}
+
+inline REAL INT_RES_11_0(REAL a, REAL b, REAL c, REAL d, REAL e, REAL f, REAL g) {
+    // * Int[(a+b x+c x^2)/(d+e x)/(f+g x),{x,-1,1}]
+    REAL den = e * e * g * g * (e * f - d * g);
+    REAL num1 = 2 * c * e * g * (e * f - d * g);
+    REAL num2 = g * g * (a * e * e - b * d * e + c * d * d) * log((d + e) / (d - e));
+    REAL num3 = e * e * (a * g * g - b * f * g + c * f * f) * log((f - g) / (f + g));
+    return (num1 + num2 + num3) / den;
+}
+
+inline REAL INT_RES_11_1(REAL a, REAL b, REAL c, REAL d, REAL e) {
+    // * Int[(a+b x+c x^2)/(d+ e x)^2,{x,-1,1}]
+    REAL den = pow(e, 3) * (e - d) * (d + e);
+    REAL num = 2 * e * (e * e * (c - a) + b * d * e - 2 * c * d * d) -
+               (d - e) * (d + e) * log((d - e) / (d + e)) * (2 * c * d - b * e);
+    return num / den;
+}
+
+REAL Scatter22_Rate::Get_Amp_Integrate_over_Phase_Space_Single_Channel(
+    const Process_Amp::NUMERATOR_STRUCTURE &numerator, const Process_Amp::DENOMINATOR_STRUCTURE &denominator) {
+    using CR = Process_Amp::CTH_RES;
+    CR n0 = numerator[0];
+    CR n1 = numerator[1];
+    CR n2 = numerator[2];
+
+    using PS = Process_Amp::PROPAGATOR_STRUCTURE;
+    PS p0 = denominator.first;
+    PS p1 = denominator.second;
+
+    int id_p0 = p0.first;
+    CR d00 = p0.second[0];
+    CR d01 = p0.second[1];
+
+    int id_p1 = p1.first;
+    CR d10 = p1.second[0];
+    CR d11 = p1.second[1];
+
+    REAL nn0 = n0.second;
+    REAL nn1 = n1.second;
+    REAL nn2 = n2.second;
+    REAL nd00 = d00.second;
+    REAL nd01 = d01.second;
+    REAL nd10 = d10.second;
+    REAL nd11 = d11.second;
+
+    // * The amplitude has following structure
+    // * (n0 + n1*cth + n2*cth^2)/(d00 + d01*cth)/(d10+d11*cth)
+    // * Then according to different situation for {d00,d01} and {d10,d11}, we will formally integral over cth
+    if (id_p0 == id_p1) {
+        // * In this case d00==d10, and d01 == d11
+        // * (n0 + n1*cth + n2*cth^2)/(d00 + d01*cth)^2
+        if (d01.first) {
+            // * (n0 + n1*cth + n2*cth^2)/(d00 + d01*cth)^2
+            return INT_RES_11_1(nn0, nn1, nn2, nd00, nd01);
+        }
+
+        if (!d01.first) {
+            // * (n0 + n1*cth + n2*cth^2)/(d00)^2
+            return INT_RES_00(nn0, nn1, nn2, nd00, nd00);
+        }
+    }
+
+    if (id_p0 != id_p1) {
+        // * (n0 + n1*cth + n2*cth^2)/(d00 + d01*cth)/(d10 + d11*cth)
+        if (d01.first && d11.first) {
+            // * (n0 + n1*cth + n2*cth^2)/(d00 + d01*cth)/(d10 + d11*cth)
+            return INT_RES_11_0(nn0, nn1, nn2, nd00, nd01, nd10, nd11);
+        }
+
+        if (d01.first && !d11.first) {
+            // * (n0 + n1*cth + n2*cth^2)/(d00 + d01*cth)/(d10)
+            return INT_RES_01(nn0, nn1, nn2, nd00, nd01, nd10);
+        }
+
+        if (!d01.first && d11.first) {
+            // * (n0 + n1*cth + n2*cth^2)/(d00)/(d10 + d11*cth)
+            return INT_RES_01(nn0, nn1, nn2, nd10, nd11, nd00);
+        }
+
+        if (!d01.first && !d11.first) {
+            // * (n0 + n1*cth + n2*cth^2)/(d00)/(d10)
+            return INT_RES_00(nn0, nn1, nn2, nd00, nd10);
+        }
+    }
+
+    return 0;
 }
