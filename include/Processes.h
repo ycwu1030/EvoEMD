c0bcac8008ff2a51e6d47a2fd6a94b03c4443be3
Yongcheng Wu <yc_wu1030@126.com>
2021-10-21 20:58:50 -0500

    wip(Processes): amplitude for N->L Phi

--- include/Processes.h
+++ include/Processes.h
@@ -15,7 +15,23 @@ public:
         FINAL.push_back(pf.Get_Particle(Particle_Factory::Phi));
         FINAL.push_back(pf.Get_Particle(Particle_Factory::DeltaL));
     };
-    virtual void Update_Amp(REAL sqrt_shat) { amp_res.n_diag[Process_Amp::SS] = 1; }
+    virtual void Update_Amp(REAL sqrt_shat) {
+        amp_res.n_diag = 1;
+
+        using PS = Process_Amp::PROPAGATOR_STRUCTURE;
+        using CRF = Process_Amp::CTH_RES_FULL;
+        // * For decay, no propagator, using dummy propagator;
+        CRF dummy_CRF(2);
+        dummy_CRF[0] = std::make_pair(true, 1);
+        dummy_CRF[1] = std::make_pair(false, 0);
+
+        PS dummy_propagator = std::make_pair(0, dummy_CRF);
+        amp_res.amps_denominator.clear();
+        amp_res.amps_denominator.push_back(std::make_pair(dummy_propagator, dummy_propagator));
+
+        // * Numerator:
+        CRF numerator(3);
+    }
     virtual REAL Get_Coeff(REAL T) { return 1; }
 };
 
