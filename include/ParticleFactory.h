f2996fdaf499940b264528c593f1efddd13d2ce3
Yongcheng Wu <yc_wu1030@126.com>
2021-10-21 22:49:54 -0500

    impr(ParticleFactory): clean the ParticleFactory
    Clean the Particle_Factory, remove any real implemented particle which should be added by users when they need them

--- include/ParticleFactory.h
+++ include/ParticleFactory.h
@@ -9,30 +9,21 @@
 
 class Particle_Factory {
 public:
-    typedef enum {
-        RHN1 = 900001,
-        RHN2 = 900002,
-        RHN3 = 900003,
-        DeltaL = 900011,
-        LNU1 = 900012,
-        Phi = 900025,
-        Chi = 900100,
-        S = 900200
-    } Particle_Name;
-    typedef std::map<Particle_Name, Pseudo_Particle *> Particle_List;
+    typedef std::map<int, Pseudo_Particle *> Particle_List;
+
     static Particle_Factory &Get_Particle_Factory();
-    Pseudo_Particle *Get_Particle(Particle_Name);
-    bool Register_POI(Particle_Name);
-    bool Set_Mass(Particle_Name PID, double mass);
+
+    Pseudo_Particle *Get_Particle(int PID);
+    bool Register_Particle(Pseudo_Particle *);
+    bool Register_POI(int PID);
+    bool Set_Mass(int PID, double mass);
 
 private:
     Particle_Factory();
     ~Particle_Factory();
 
-    Particle_List PL;             // All Particle
-    std::set<Particle_Name> POI;  // Particle of Interested
-
-    Pseudo_Particle *Register_Default_Particle(Particle_Name);
+    Particle_List PL;   // All Particle
+    std::set<int> POI;  // Particle of Interested
 };
 
 #endif  //_PARTICLE_FACTORY_H_
