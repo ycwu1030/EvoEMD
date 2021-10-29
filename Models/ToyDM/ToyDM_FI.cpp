c84b595264bc42d593e10c41d470fc855185f9d1
Yongcheng Wu <yc_wu1030@126.com>
2021-10-29 08:50:28 -0500

    fix(RungeKutta): Fix the bug when checking whether a freeze-in like process need to be compensate to equilibrium
    Fix the bug when checking whether a freeze-in like process become equilibrium
    Add example for freeze-out and freeze-in Dark Matter

--- Models/ToyDM/ToyDM_FI.cpp
+++ Models/ToyDM/ToyDM_FI.cpp
@@ -1,4 +1,4 @@
-#include "ToyDM.h"
+#include "ToyDM_FI.h"
 
 #include <cmath>
 #include <iostream>
@@ -9,20 +9,21 @@ using namespace EvoEMD;
 using namespace std;
 int main(int argc, char const *argv[]) {
     Parameter_Base *tr = RETRIVE_PARAMETER(Tr);
-    tr->Set_Value(1e6);
+    tr->Set_Value(1);
+    REAL Ti = GET_PARAM_VALUE(Ti);
     Parameter_Base *mx = RETRIVE_PARAMETER(MX);
     BoltzmannEquation BE(mx);
     REAL scale = mx->Get_Value();
     REAL T_BEGIN = 100 * scale;
-    REAL T_END = scale / 1000.0;
+    REAL T_END = 1e-2;
     BE.Set_X_Range(log(scale / T_BEGIN), log(scale / T_END));
     // BE.Set_X_BEGIN(log(scale / T_BEGIN) + 1e-6);
     cout << "Solving for [" << BE.Get_X_BEGIN() << "," << BE.Get_X_END() << "]" << endl;
     cout << "DOF = " << BE.Get_DOF() << endl;
     cout << "Starting from: Y = " << BE.Get_Y_BEGIN()[0] << endl;
+    Hubble_History::Get_Hubble_History().Print_History();
     RungeKutta rk(&BE);
-    cout << "System Built" << endl;
     rk.Solve(1e-4);
-    rk.Dump_Solution("ToyDM_Result.txt");
+    rk.Dump_Solution("ToyDM_FI_Result.txt");
     return 0;
 }
--- Models/ToyDM/ToyDM_FO.h
+++ Models/ToyDM/ToyDM_FO.h
@@ -6,7 +6,7 @@
 using namespace EvoEMD;
 
 // * Free Parameters
-DECLARE_FREE_PARAMETER(Lam, 1e-3);
+DECLARE_FREE_PARAMETER(Lam, 0.8);
 DECLARE_FREE_PARAMETER(MX, 100);
 
 // * All Particles
@@ -14,7 +14,7 @@ REGISTER_PARTICLE(Boson, X, 900001, 2, RETRIVE_PARAMETER(MX), nullptr);
 REGISTER_PARTICLE(Boson, S, 900011, 1, nullptr, nullptr);
 
 // * Particles entering the Boltzmann Equation
-REGISTER_POI(900001, 0);
+REGISTER_POI(900001, 1);
 
 // * Regsiter Processes
 REGISTER_PROCESS(XX_SS_Amp);
